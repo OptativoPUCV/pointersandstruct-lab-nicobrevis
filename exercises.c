35a609b94d4ba651832394538462d7d8fd82fb7b
Replit user <replituser@example.com>
2021-03-21 01:13:54 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -135,8 +135,8 @@ void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
   Vector* b = (Vector*) calloc(2,sizeof(Vector));
   b->datos = (int*)calloc(2,sizeof(int));
   asignarValor( a , 0 , a1 );
-  asignarValor( a , 1 , b1 );
-  asignarValor( b , 0 , a2 );
+  asignarValor( a , 1 , a2 );
+  asignarValor( b , 0 , b1 );
   asignarValor( b , 1 , b2 );
  sumaV(a,b,c);
  
