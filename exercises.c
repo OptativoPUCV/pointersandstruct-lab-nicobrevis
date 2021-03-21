d03ef32fdfe88f4045fe72ad6e59477fa4c3a39c
Replit user <replituser@example.com>
2021-03-21 00:31:55 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -130,10 +130,7 @@ sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
 
-crearVector(2);
-int a = obtenerValor( c, a1+a2);
-int b = obtenerValor(c, b1+b2);
-c->datos = &a + b;
+
 
 
 }
